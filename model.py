3e56968c6937c4ada0a53bbc1c28f980363464a3
Jacob-Spiess <68386966+Jacob-Spiess@users.noreply.github.com>
2023-01-02 16:48:43 +0100

    model.estimate opponent

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import numpy as np
 from matrix_generator import MatrixGenerator
 from particle import Particle
-from nashpy import Game
+#from nashpy import Game
 
 class Model:
     def __init__(self, numberOfParticles, reciprocationLevel, errorLevels, nb_moves, pertubationFactor=0.2,
@@ -21,18 +21,19 @@ class Model:
         self.particles = [[np.random.normal(), np.random.normal(),
                            np.random.randint(low=0, high=self.matrix.nb_lemke_howson_labels())] for _ in range(0, numberOfParticles)]
 
-    def update_error_estimates(believeOpponent, attitudeOpponent, agentPayoffMatrix, opponentPayoffMatrix):
+    def update_error_estimates(believeOpponent, attitudeOpponent, game):
 
         attitudeAgent = believeOpponent
-
-        modded_game = modify_game(game, attitudeAgent, attitudeOpponent)
-        ne_agent, ne_opp = nash_opp(modded_game)
+'''
+        moddified_game = modify_game(game, attitudeAgent, attitudeOpponent)
+        ne_agent, ne_opp = nash_opponent(moddified_game)
         j = ne_opp[opp_move]
         k = coop(att_opp, bel_opp)
         for l, lvl in enumerate(err_lvls):
             err_distr[str(lvl)] = err_distr[str(lvl)] # * t(j, k, l)
         normalize_dictionary(err_distr)
         err = est_error(err_lvls, err_distr)
+'''
 
     def update_resample_particles():
         return
@@ -40,11 +41,25 @@ class Model:
     def update_perturb_particles():
         return
 
-    def modify_game(attitudeAgent, attitudeOpponent, game):
-        agentPayoffMatrix, opponentPayoffMatrix = game.payoff_matrices
+    def nash_opponent():
 
-        agentModifiedPayOffMatrix = agentPayoffMatrix + attitudeAgent*opponentPayoffMatrix
-        opponentModifiedPayOffMatrix = opponentPayoffMatrix + attitudeOpponent*agentPayoffMatrix
+        return
 
-        return agentModifiedPayOffMatrix, opponentModifiedPayOffMatrix
+    def estimate_opponents_method(self):
+        numbers = [p[2] for p in self.particles]
+
+
+        particle = self.particles[]
+        nash_opponent = particle[2]
+        return nash_opponent
+
+
+def modify_game(attitudeAgent, attitudeOpponent, game):
+
+    agentPayoffMatrix, opponentPayoffMatrix = game.payoff_matrices
+
+    agentModifiedPayOffMatrix = agentPayoffMatrix + attitudeAgent*opponentPayoffMatrix.T
+    opponentModifiedPayOffMatrix = opponentPayoffMatrix + attitudeOpponent*agentPayoffMatrix.T
+
+    return agentModifiedPayOffMatrix, opponentModifiedPayOffMatrix
          
