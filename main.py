e25e0de3590935940fd8fb6a9f6ad1ad694b32c8
Jacob-Spiess <68386966+Jacob-Spiess@users.noreply.github.com>
2023-01-02 18:46:08 +0100

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from model import Model
+from quantecon.game_theory import NormalFormGame
+import numpy as np
 
 def main():
     n = 200
@@ -6,10 +8,16 @@ def main():
     f_ab = 0.2
     f_nash = 0.1
     err_lvls = [.0, .001, .002, .004, .008, .016, .032, .064, .128, .256, .512, 1.0]
-    nb_moves = 16
+    nb_moves = 2
 
     model = Model(n, r, err_lvls, nb_moves, f_ab, f_nash)
-    print(model.particles)
+    A, B = model.matrix.random_matrix()
+
+    A, B = np.array([[1,2],[3,4]]), np.array([[5,6],[7,8]])
+    normalFormGame = NormalFormGame(A, B)
+    print(normalFormGame)
+
+    print(model.modify_game(-1,0,normalFormGame))
 
 
 
--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import numpy as np
 from matrix_generator import MatrixGenerator
 from particle import Particle
 #from nashpy import Game
+from quantecon.game_theory import lemke_howson, NormalFormGame, Player
 
 class Model:
     def __init__(self, numberOfParticles, reciprocationLevel, errorLevels, nb_moves, pertubationFactor=0.2,
@@ -21,19 +22,24 @@ class Model:
         self.particles = [[np.random.normal(), np.random.normal(),
                            np.random.randint(low=0, high=self.matrix.nb_lemke_howson_labels())] for _ in range(0, numberOfParticles)]
 
-    def update_error_estimates(believeOpponent, attitudeOpponent, game):
+    def update_error_estimates(self, believeOpponent, attitudeOpponent, normalFormGame):
 
         attitudeAgent = believeOpponent
-'''
-        moddified_game = modify_game(game, attitudeAgent, attitudeOpponent)
-        ne_agent, ne_opp = nash_opponent(moddified_game)
-        j = ne_opp[opp_move]
-        k = coop(att_opp, bel_opp)
-        for l, lvl in enumerate(err_lvls):
-            err_distr[str(lvl)] = err_distr[str(lvl)] # * t(j, k, l)
-        normalize_dictionary(err_distr)
-        err = est_error(err_lvls, err_distr)
-'''
+
+        modifiedGame = self.modify_game(normalFormGame, attitudeAgent, attitudeOpponent)
+ 
+        starting_label = self.estimate_opponents_method()
+        _, nash_opponent = lemke_howson(modifiedGame, starting_label)
+
+
+#        ne_agent, ne_opp = nash_opponent(moddified_game)
+#        j = ne_opp[opp_move]
+#        k = coop(att_opp, bel_opp)
+#        for l, lvl in enumerate(err_lvls):
+#            err_distr[str(lvl)] = err_distr[str(lvl)] # * t(j, k, l)
+#        normalize_dictionary(err_distr)
+#        err = est_error(err_lvls, err_distr)
+
 
     def update_resample_particles():
         return
@@ -41,25 +47,26 @@ class Model:
     def update_perturb_particles():
         return
 
-    def nash_opponent():
-
-        return
 
     def estimate_opponents_method(self):
         numbers = [p[2] for p in self.particles]
+        counts = dict()
+        for n in numbers:
+            if str(n) in counts:
+                counts[str(n)] += 1
+            else:
+                counts[str(n)] = 1
+        keys = list(counts.keys())
+        values = np.array(list(counts.values()))
+        max_id = np.argmax(values)
+        return keys[max_id]
 
 
-        particle = self.particles[]
-        nash_opponent = particle[2]
-        return nash_opponent
+    def modify_game(self, attitudeAgent, attitudeOpponent, game: NormalFormGame):
+        agentPayoffMatrix, opponentPayoffMatrix = game.players[0].payoff_array, game.players[1].payoff_array
 
+        agentModifiedPayOffMatrix = agentPayoffMatrix + attitudeAgent * opponentPayoffMatrix.T
+        opponentModifiedPayOffMatrix = opponentPayoffMatrix + attitudeOpponent * agentPayoffMatrix.T
 
-def modify_game(attitudeAgent, attitudeOpponent, game):
+        return NormalFormGame(agentModifiedPayOffMatrix, opponentModifiedPayOffMatrix)
 
-    agentPayoffMatrix, opponentPayoffMatrix = game.payoff_matrices
-
-    agentModifiedPayOffMatrix = agentPayoffMatrix + attitudeAgent*opponentPayoffMatrix.T
-    opponentModifiedPayOffMatrix = opponentPayoffMatrix + attitudeOpponent*agentPayoffMatrix.T
-
-    return agentModifiedPayOffMatrix, opponentModifiedPayOffMatrix
-         
